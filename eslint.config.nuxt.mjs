e4d112b3827a86765b031bffe3903473f05960df
Genbu Hase <14960841+GenbuHase@users.noreply.github.com>
2025-12-27 01:46:59 +0900

    [Fix] ESLint Nuxt Configのstylisticをfalseに変更 $ Prettierとの競合を避けるため

--- eslint.config.nuxt.mjs
+++ eslint.config.nuxt.mjs
@@ -2,15 +2,9 @@
 import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 import prettier from "eslint-config-prettier";
 
-/*
- Note: In a real Nuxt project, you should often rely on the module `@nuxt/eslint` to generate the config.
- This file demonstrates how to manually use `@nuxt/eslint-config` with flat config if needed,
- or can be used as a reference for extending the generated config.
-*/
-
 export default createConfigForNuxt({
 	features: {
 		tooling: true,
-		stylistic: true
+		stylistic: false
 	}
 }).prepend(prettier);
